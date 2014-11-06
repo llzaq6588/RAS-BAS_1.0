09a4fd6aeddd7e3b87505a2445a0b515c4cfd2df
llzaq6588 <llzaq6588@naver.com>
2014-11-06 13:12:38 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+echo "=============================="
+echo "=====  RAS-BAS INSTALL   ====="
+echo "=============================="
+
+apt-get update
+apt-get upgrade
+
+apt-get install apache2
+apt-get install libapache2-mod-php5 php5
+apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+apt-get install mc
+apt-get install vim
+
+clear
+
+echo "=============================="
+echo "===== INSTALL WEB SERVER ====="
+echo "=============================="
+
+apt-get install git-core
+
+git clone git://git.drogon.net/wiringPi
+
+cd wiringPi
+./build
+
+clear
+
+echo "=============================="
+echo "=====    INSTALL GPIO    ====="
+echo "=============================="
+
+gpio -v gpio readall
+
+clear
+
+git clone https://github.com/llzaq6588/RAS-BAS_1.0
+
+cd ./RAS-BAS_1.0
+
 cp -r ./rasbas /var
 chown root /var/rasbas/ -R
 chgrp root /var/rasbas/ -R
@@ -9,7 +50,7 @@ sudo cp -r ./www /var
 mkdir /var/www/phpmyadmin
 cp /var/www/phpmyadmin.zip /var/www/phpmyadmin
 cd /var/www/phpmyadmin
-sudo unzip .//phpmyadmin.zip
+sudo unzip ./phpmyadmin.zip
 
 /etc/init.d/apache2 restart
 
