12a2e09bb222f7da77274d0051619915c35fdb7d
KAERIUS <llzaq6588@naver.com>
2014-11-06 17:39:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ clear
 cp -r ./rasbas /var
 chown root /var/rasbas/ -R
 chgrp root /var/rasbas/ -R
-chmod 755 /var/rasbas/ -R
+chmod 500 /var/rasbas/ -R
 
 sudo cp -r ./www /var
 mkdir /var/www/phpmyadmin
